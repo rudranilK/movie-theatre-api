1eb66d98e7cd188a5c50651f2e9af2504e9f781e
rudranilK <rudranilkundu@gmail.com>
2024-03-19 19:20:09 +0530

    removed WIP comments and commented code

--- src/model/row.js
+++ src/model/row.js
@@ -20,18 +20,6 @@ const rowSchema = new mongoose.Schema({
    }
 });
 
-//WIP - validate capacity is full or not
-// rowSchema.pre('findOneAndUpdate', function (next) {
-//    const update = this.getUpdate();
-//    const filledSeats = update.$set ? update.$set.filled_seats : undefined;
-
-
-//    if (filledSeats && filledSeats.length > this.seats.length) {
-//       throw new Error('Seats for this row are full!');
-//    }
-//    next();
-// });
-
 const Row = mongoose.model('row', rowSchema);
 
 module.exports = {
--- src/service/seat.js
+++ src/service/seat.js
@@ -27,26 +27,11 @@ module.exports.bookSeat = async function (req, res) {
 
    //Seat is not booked - 1. check for seats +- 1 if booked, -> can't book seat
    const surroundingSeats = await Seat.find({
-      // $and: [
-      //    { row_no },
-      //    {
-      //       seat_no: {
-      //          $in: [Number(seat_no) - 1, Number(seat_no) + 1]
-      //       }
-      //    }
-      // ]
       row_no,
       seat_no: {
          $in: [Number(seat_no) - 1, Number(seat_no) + 1]
       }
    }, { seat_no: 1, row_no: 1, is_booked: 1 });
-
-   //! The return only terminates the loop and not the parent function - chatGPT 
-   // surroundingSeats?.forEach(el => {
-   //    if (el.is_booked) {
-   //       return res.status(400).json({ data: null, message: `Sorry the adjacent seat, Seat: ${el.seat_no}, Row: ${seat.row_no} is booked` });
-   //    }
-   // });
 
    if (surroundingSeats.some(el => el.is_booked)) {
       return res.status(400).json({ data: null, message: `Sorry the adjacent seat is booked` });
@@ -54,39 +39,6 @@ module.exports.bookSeat = async function (req, res) {
 
    // Seat can be booked;
    const row = await Row.findOne({ _id: seat.row });
-
-   //! WIP - Transactions - need to use replica set for transactions
-
-   // const session = await mongoose.startSession();
-   // session.startTransaction();
-   // try {
-
-   //    const opts = { session };
-
-   //    await Seat.updateOne({ _id: seat._id }, {
-   //       $set: { is_booked: true }
-   //    }, opts);
-
-   //    await Row.updateOne({ _id: row._id }, {
-   //       $set: {
-   //          filled_seats: {
-   //             $push: seat.seat_no
-   //          },
-   //          is_full: ((row.filled_seats.length + 1) === row.seats.length)
-   //       }
-   //    }, opts);
-
-   //    await session.commitTransaction();
-   //    session.endSession();
-   // } catch (error) {
-   //    // If an error occurred, abort the whole transaction and
-   //    // undo any changes that might have happened
-   //    await session.abortTransaction();
-   //    session.endSession();
-   //    throw error;
-   // }
-
-   // const updatedSeat = await Seat.findOne({ _id: seat._id });
 
    const updatedSeat = await Seat.findOneAndUpdate({ _id: seat._id }, {
       $set: { is_booked: true }
